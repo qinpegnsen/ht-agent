8b37f6c33d727f561e699b8bbb8167cdeeb98663
qps <744056574@qq.com>
2017-09-04 14:20:41 +0800

    add 添加核心服务

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, HostBinding, OnInit } from '@angular/core';
 declare var $: any;
 
 import { SettingsService } from './core/settings/settings.service';
+import {isNullOrUndefined} from "util";
+import {ToasterConfig,ToasterService} from "angular2-toaster";
 
 @Component({
     selector: 'app-root',
@@ -9,7 +11,16 @@ import { SettingsService } from './core/settings/settings.service';
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-
+    public toaster: ToasterConfig = new ToasterConfig({
+      showCloseButton: true,
+      tapToDismiss: false,
+      timeout: {'success': 3000, 'error': 0},
+      positionClass: 'toast-top-center',
+      animationClass: 'slide-from-top'
+    });
+    static toasterConfig:ToasterConfig; //消息提示配置
+    static toasterService:ToasterService; //消息提示服务
+    //动态样式
     @HostBinding('class.layout-fixed') get isFixed() { return this.settings.layout.isFixed; };
     @HostBinding('class.aside-collapsed') get isCollapsed() { return this.settings.layout.isCollapsed; };
     @HostBinding('class.layout-boxed') get isBoxed() { return this.settings.layout.isBoxed; };
@@ -26,4 +37,15 @@ export class AppComponent implements OnInit {
     ngOnInit() {
         $(document).on('click', '[href="#"]', e => e.preventDefault());
     }
+  /**
+   * 消息提醒弹框
+   * @param type 类型：error、success、info...
+   * @param title 提示头信息
+   * @param info 内容信息
+   * @param operation 参数信息
+   */
+  static rzhAlt (type: string, title: string, info?: string, operation?: any,appcom?:AppComponent) {
+    if (!isNullOrUndefined(operation)) appcom.toaster = new ToasterConfig(operation);
+    AppComponent.toasterService.pop(type, title, info);
+  }
 }
--- src/app/core/settings/settings.service.ts
+++ src/app/core/settings/settings.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import {isNullOrUndefined} from "util";
+import { Location }from '@angular/common';
 declare var $: any;
 
 @Injectable()
@@ -8,7 +10,7 @@ export class SettingsService {
     public app: any;
     public layout: any;
 
-    constructor() {
+    constructor(private location: Location) {
 
         // User Settings
         // -----------------------------------
@@ -77,5 +79,41 @@ export class SettingsService {
     toggleLayoutSetting(name) {
         return this.setLayoutSetting(name, !this.getLayoutSetting(name));
     }
+  /**
+   * 显示右侧页面 by 立坤
+   * width是定义pc端下宽度，不传默认宽度是50%，可以传“30%”表示百分比宽度，可以传“500px”表示固定宽度
+   * @param width
+   */
+  showRightPage(width?) {
+    let me = this;
+    setTimeout(() => {
+      if ($(window).width() > 768 && !isNullOrUndefined(width)) $('.rightpage').css('width', width); //pc模式下,可以自定义宽度
+      me.layout.operationpageOpen = true;  //开启右侧页面
+      $('html').removeClass('csstransforms3d'); //剔除动画效果，此效果和浮动冲突
+      $('app-offsidebar').hide();
+    }, 10);
+  }
 
+  /**
+   * 关闭右侧页面 by 立坤
+   * @param
+   */
+  closeRightPage() {
+    this.layout.operationpageOpen = false; //关闭右侧滑动页面
+    $('html').addClass('csstransforms3d'); //加入样式，动态滑动效果
+    $('app-offsidebar').show();
+  }
+
+  /**
+   * 关闭右侧页面并返回上级路由
+   * 针对路由跳转的右弹窗
+   * by 高洁
+   */
+  closeRightPageAndRouteBack(){
+    this.closeRightPage();//关闭右侧滑动页面
+    let that = this;
+    setTimeout(function(){
+      that.location.back();//返回上级路由
+    },100)
+  }
 }
