1b8505682b89ad3f841f81c81ce278754b80e502
qps <744056574@qq.com>
2017-09-04 16:33:50 +0800

    add 进货管理

---
+++ src/app/routes/stock-man/stock-man.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-man',
+  templateUrl: './stock-man.component.html',
+  styleUrls: ['./stock-man.component.scss']
+})
+export class StockManComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
