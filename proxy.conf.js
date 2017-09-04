b1549ced10e55567c09fc799855d59da6ae1fc11
qps <744056574@qq.com>
2017-09-04 16:26:38 +0800

    add 跨域解决

---
+++ proxy.conf.js
@@ -0,0 +1,15 @@
+/**
+ * Created by qinpengsen on 2017/9/4.
+ */
+const PROXY_CONFIG = [
+  {
+    context: [
+
+    ],
+    target: "",   //拦截 context配置路径，经过此地址
+    secure: false
+  }
+
+];
+
+module.exports = PROXY_CONFIG;
