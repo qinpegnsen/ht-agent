e1a989af1c4c6f8b941cdc11a0a01e87f8ffeadc
yanweiyi <m15901019392_1@163.com>
2017-09-12 11:05:36 +0800

    up 修改代理商收货地址

--- proxy.conf.js
+++ proxy.conf.js
@@ -21,7 +21,7 @@ const PROXY_CONFIG = [
       "/login",
       "/admin",
     ],
-    target: "http://192.168.10.167:8901",   //拦截 context配置路径，经过此地址
+    target: "http://192.168.10.109:8088",   //拦截 context配置路径，经过此地址
     secure: false
   }
 ];
