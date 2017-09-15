eb7b3baddbc0b3f84a45ad9a267fd74384055832
qps <744056574@qq.com>
2017-09-15 09:48:18 +0800

    不提交

--- proxy.conf.js
+++ proxy.conf.js
@@ -4,16 +4,17 @@
 const PROXY_CONFIG = [
   {
     context: [
-      "/agent"
+      "/agent",
+      "/nativeWXPay"
     ],
-    target: "http://192.168.10.109:8088",   //拦截 context配置路径，经过此地址
+    target: "http://192.168.10.167:8901",   //拦截 context配置路径，经过此地址
     secure: false
   },
   {
     context: [
       "/goodsQuery"
     ],
-    target: "http://192.168.10.167:8901",   //拦截 context配置路径，经过此地址
+    target: "http://192.168.10.109:8088",   //拦截 context配置路径，经过此地址
     secure: false
   },
   {
