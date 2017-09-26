95cf9086555c88b7ffef5f64c865a4b65c4bb2c8
qps <744056574@qq.com>
2017-09-26 20:02:39 +0800

    add 服务

--- src/app/routes/stock-man/ord-record/order-detail/order-detail.component.ts
+++ src/app/routes/stock-man/ord-record/order-detail/order-detail.component.ts
@@ -97,6 +97,7 @@ export class OrderDetailComponent implements OnInit {
       ordno:'1234123451235'                //目前是写死的，以后再改
     };
     this.LogisticsData=this.stockManService.getShopList(url,data);
+    console.log("█ this.LogisticsData ►►►",  this.LogisticsData);
     this.express=this.LogisticsData[1];
   }
 
