fd97109c68a5cc9242d080110d21faef2d4773c4
qps <744056574@qq.com>
2017-09-26 19:51:45 +0800

    add 服务

--- src/app/core/services/rzhtools.service.ts
+++ src/app/core/services/rzhtools.service.ts
@@ -444,14 +444,9 @@ export class RzhtoolsService {
    * @returns {string}
    */
   static getWeek = function (date: Date, lan?) {
-    console.log("█ date ►►►",  date);
     let today = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
     if (!isUndefined(lan) && lan == 'cn') today = new Array('周一', '周二', '周三', '周四', '周五', '周六', '周日');
     let week = today[date.getDay()];
-    // let weeks: Array<any> = this.getEnumDataList(SettingsService.enums.week), week: string, num: number = date.getDay() + 1;
-    // for (let i = 0; i < weeks.length; i++) {
-    // 	if (num.toString() == weeks[i]["val"]) week = weeks[i]["key"];
-    // }
     return week;
   }
 
