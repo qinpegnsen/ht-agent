94f3099d9067d929b0c547075f7be4b2ebfe579e
qps <744056574@qq.com>
2017-10-17 10:29:42 +0800

    up 代理商进货

--- src/app/routes/message-inform/message-inform.module.ts
+++ src/app/routes/message-inform/message-inform.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { MessageInformComponent } from './message-inform/message-inform.component';
 import {RouterModule, Routes} from "@angular/router";
 import {MessageInformService} from "./message-inform.service";
+import {SharedModule} from "../../shared/shared.module";
 
 const routes: Routes = [
   {path: '', component:MessageInformComponent},
@@ -11,6 +12,7 @@ const routes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
+    SharedModule.forRoot(), // 公用模块
     RouterModule.forChild(routes)
   ],
   declarations: [MessageInformComponent],
