e00c7e8e5851dde424aecb54db30c026aae4f204
qps <744056574@qq.com>
2017-11-10 10:37:10 +0800

    样式

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component,OnInit} from '@angular/core';
 import {NavigationStart, Router} from "@angular/router";
 
 @Component({
@@ -6,14 +6,13 @@ import {NavigationStart, Router} from "@angular/router";
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
-export class LayoutComponent implements OnInit,OnDestroy{
+export class LayoutComponent implements OnInit{
   private menuItems: any;
   private path;
   public carNum: number;
-  public urlChange;                      //路由的变化，用来取消订阅
 
   constructor(private router: Router) {
-    this.urlChange=this.router.events
+    this.router.events
       .filter(event => event instanceof NavigationStart)
       .subscribe((event) => {
         this.path = event['url'];
@@ -22,12 +21,5 @@ export class LayoutComponent implements OnInit,OnDestroy{
 
   ngOnInit() {
 
-  }
-
-  /**
-   * 取消订阅
-   */
-  ngOnDestroy(){
-    this.urlChange.unsubscribe();
   }
 }
