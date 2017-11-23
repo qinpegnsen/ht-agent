183cd24b1d90271b7c9b95a36cfb1770b76e3ce0
qps <744056574@qq.com>
2017-11-23 14:12:24 +0800

    不提交

--- proxy.conf.js
+++ proxy.conf.js
@@ -5,6 +5,7 @@ const sg = 'http://192.168.10.111:';  //善谷
 const zyg = 'http://192.168.10.167:'; //张阳光
 const gh = 'http://192.168.10.109:';  //高辉
 const wp = 'http://192.168.10.182:';  //万鹏
+const csj = 'http://192.168.10.221:';  //测试机
 
 
 /**
@@ -23,9 +24,10 @@ const PROXY_CONFIG = [
       "/statistical",
       "/datadict",
       "/finaceDraw",
-      "/notifyAgent/"
+      "/notifyAgent",
+      "/rpAccountRec"
     ],
-    target: zyg + "8085",   //拦截 context配置路径，经过此地址
+    target: gh + "8085",   //拦截 context配置路径，经过此地址
     secure: false
   },
   {
@@ -34,7 +36,7 @@ const PROXY_CONFIG = [
       "/res",
       "/upload"
     ],
-    target: zyg + "8082",   //拦截 context配置路径，经过此地址
+    target: gh + "8082",   //拦截 context配置路径，经过此地址
     secure: false
   }
 ];
